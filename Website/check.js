a7fb6205ec8570e6638f2d219f5f18fd77dfbb97
Erraseus <92537934+Erraseus@users.noreply.github.com>
2021-10-17 11:57:47 +0200

    Copy paste save

--- Website/check.js
+++ Website/check.js
@@ -16,3 +16,27 @@ function Check(){
 		}
 	}
 }
+
+$(document).ready(function() {
+    var ctrlDown = false,
+        ctrlKey = 17,
+        cmdKey = 91,
+        vKey = 86,
+        cKey = 67;
+
+    $(document).keydown(function(e) {
+        if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+    }).keyup(function(e) {
+        if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+    });
+
+    $(".no-copy-paste").keydown(function(e) {
+        if (ctrlDown && (e.keyCode == vKey || e.keyCode == cKey)) return false;
+    });
+    
+    // Document Ctrl + C/V 
+    $(document).keydown(function(e) {
+        if (ctrlDown && (e.keyCode == cKey)) window.location = "https://www.youtube.com/watch?v=AGvrDe3rKxA";
+        if (ctrlDown && (e.keyCode == vKey)) window.location = "https://www.youtube.com/watch?v=AGvrDe3rKxA";
+    });
+});
